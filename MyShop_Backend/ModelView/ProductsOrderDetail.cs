ea92bb8022fc37bde8ec553370a79836b5b0d3f5
LeThiHongNgoc <ngocb2005766@student.ctu.edu.vn>
2024-10-08 17:46:46 +0700

    ModelView

---
+++ MyShop_Backend/ModelView/ProductsOrderDetail.cs
@@ -0,0 +1,10 @@
+﻿namespace MyShop_Backend.ModelView
+{
+	public class ProductsOrderDetail
+	{
+		public string ProductName { get; set; }
+		public string Size { get; set; }
+		public int Quantity { get; set; }
+		public double UnitPrice { get; set; }
+	}
+}
